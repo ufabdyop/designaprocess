c787729469bdd3ee516e9d6045a2765b95d36e8b
Ryan Taylor <ryant@nanofab.utah.edu>
2012-08-21 11:45:55 -0600

    open print-friendly version in new window

--- design_your_process/runcard/view_runcard.php
+++ design_your_process/runcard/view_runcard.php
@@ -13,7 +13,7 @@ foreach($process_forms as $pf) {
     echo $pf->to_html();
 }
 echo "<br/>";
-echo styled_button("print_runcard.php?id=$runcard_id", "Printer-Friendly Version", "print");
+echo styled_button("print_runcard.php?id=$runcard_id", "Printer-Friendly Version", "print", array(), array('_target' => 'blank'));
 ?>
 
 
